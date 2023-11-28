369ea8001d15419a836191c838aaf5dc57f86956
Melkor <eduardoaugustogomes@gmail.com>
2023-11-28 20:42:57 -0300

    Pequenos ajustes e retorno de log no console

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -9,6 +9,7 @@ module.exports = function Controller(schema) {
             res.json(itens)
         },
         add: async (req, res) => {
+            console.log(req.body)
             const errors = validationResult(req)
             if (!errors.isEmpty())
                 return res.status(400)
