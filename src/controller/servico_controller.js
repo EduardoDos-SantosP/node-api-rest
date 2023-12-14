26910e5eb2343239c7abc8976aaeaf9f1a4dcf39
EduardoDos-SantosP <66569234+EduardoDos-SantosP@users.noreply.github.com>
2023-12-14 18:55:52 -0300

    Alterações na criação de serviço

--- src/controller/servico_controller.js
+++ src/controller/servico_controller.js
@@ -1,6 +1,11 @@
 const Controller = require('./controller.js');
 const Servico = require('../model/servico.js')
 
+const base = Controller(Servico)
 module.exports = {
-    ...Controller(Servico)
+    ...base,
+    async add(req, res) {
+        req.body.idPrestador ??= req.usuario._id
+        return base.add(req, res)
+    }
 }
